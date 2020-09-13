a4c146077d418f89eb0fb230b896d78247c70dc2
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-13 09:18:14 +0300

    Ticket class implemented

--- src/businessLogic/Ticket.java
+++ src/businessLogic/Ticket.java
@@ -1,10 +1,79 @@
 package businessLogic;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Ticket {
 
     private FullName fullName;
     private String visitingReason;
+    private ArrayList<Doctor> doctors;
     private Calendar visitingDate;
+
+    public Ticket() {
+        fullName = new FullName();
+        visitingReason = "";
+        doctors = new ArrayList<>();
+        visitingDate = new GregorianCalendar();
+    }
+
+    public Ticket(FullName fullName, String visitingReason, ArrayList<Doctor> doctors, GregorianCalendar visitingDate) {
+        this.fullName = fullName;
+        this.visitingReason = visitingReason;
+        this.doctors = doctors;
+        this.visitingDate = visitingDate;
+    }
+
+    public Ticket(Ticket ticket) {
+        fullName = ticket.fullName;
+        visitingReason = ticket.visitingReason;
+        doctors = ticket.doctors;
+        visitingDate = ticket.visitingDate;
+    }
+
+    public FullName getFullName() {
+        return fullName;
+    }
+
+    public String getVisitingReason() {
+        return visitingReason;
+    }
+
+    public ArrayList<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public Calendar getVisitingDate() {
+        return visitingDate;
+    }
+
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setVisitingReason(String visitingReason) {
+        this.visitingReason = visitingReason;
+    }
+
+    public void setDoctors(ArrayList<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public void setVisitingDate(Calendar visitingDate) {
+        this.visitingDate = visitingDate;
+    }
+
+    @Override
+    public String toString() {
+        String result =  "Name: " + fullName + "\nVisiting reason: " + visitingReason + "\nDoctors: ";
+        StringBuilder doctorsString = new StringBuilder();
+        for (Doctor doctor: doctors) {
+            doctorsString.append(doctor.getType()).append(", ");
+        }
+        doctorsString.deleteCharAt(doctorsString.length() - 1);
+        result += doctorsString.toString();
+        result += "\nVisiting date: " + visitingDate.getTime();
+        return result;
+    }
 }
